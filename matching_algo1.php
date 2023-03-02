d402a54ff4fcf63deb1fff5bc6a2181e5f16b4ea
Raj shahane <pushkraj.shahane1234@gmail.com>
2023-03-02 10:53:44 +0530

    muaz

--- matching_algo1.php
+++ matching_algo1.php
@@ -191,7 +191,7 @@ $result = mysqli_query($connect,$sql);
  if (mysqli_num_rows($result) > 0) 
  {  
       while($row = mysqli_fetch_array($result))
-          //$row = $result->fetch_assoc())  
+          // $row = $result->fetch_assoc())  
       {  
  ?>  
 
@@ -212,5 +212,5 @@ $result = mysqli_query($connect,$sql);
 </div>  
 </div>  
 <br />  
-</body>  
+</body>  
 </html>
